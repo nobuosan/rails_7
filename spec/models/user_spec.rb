525d08d5daa0b2adb7cfac0d8d48f80c0e8130ac
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-03-17 01:32:45 +0900

    --test-- use let command

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "名前の表示" do
+
+    let(:params) {{name:"田中"}}
+
     it "１歳：名前が「〜ちゃん」と取得できること" do
-      user = User.new(name:"田中",age:1)
+    params.merge!(age:1)
+      user = User.new(params)
       expect(user.disp_name).to eq "田中ちゃん"
     end
     it "１５歳：名前が「〜くん」と取得できること" do
-      user = User.new(name:"田中",age:15)
+      params.merge!(age:15)
+      user = User.new(params)
       expect(user.disp_name).to eq "田中くん"
     end
     it "２０歳：名前が「〜さん」と取得できること" do
-      user = User.new(name:"田中",age:20)
+      params.merge!(age:20)
+      user = User.new(params)
       expect(user.disp_name).to eq "田中さん"
     end
     it "-1歳：名前が「不正な値です」と取得できること" do
-      user = User.new(name:"田中",age:-1)
+      params.merge!(age:-1)
+      user = User.new(params)
       expect(user.disp_name).to eq "不正な値です"
     end
   end
